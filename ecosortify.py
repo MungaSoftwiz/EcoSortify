10a6819dc8f27cda8bcd08a85a21c64fee2eb23b
munga <mwachilumobm@gmail.com>
2024-03-11 16:17:26 +0300

    chore: create app instance

---
+++ ecosortify.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Defines flask application instance and tasks to manage app """
+import os
+from app import create_app
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
