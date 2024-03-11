7d4eec82e5460fb5eb28df8988488779cd7529b5
munga <mwachilumobm@gmail.com>
2024-03-11 15:55:58 +0300

    chore: add init file

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Create application instance """
+from flask import Flask
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap5()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    """ Creates an app """
+    app = Flask(__name__)
+
+    """ Configure application """
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    """ Initialize extensions """
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    """ Register blueprints """
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
