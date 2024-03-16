4431404326b5fe90ecde3ed887339e0dd9dadfc5
munga <mwachilumobm@gmail.com>
2024-03-16 18:29:17 +0300

    chore: add a shell context processor

--- ecosortify.py
+++ ecosortify.py
@@ -4,3 +4,11 @@ import os
 from app import create_app
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+
+@app.shell_context_processor
+def create_shell_context():
+    """ Automate importing of the database instance and models
+    when a flask shell session starts
+    """
+    return dict(db=db, User=User, Post=Post, Comment=Comment)
