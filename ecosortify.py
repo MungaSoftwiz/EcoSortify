f97e11ff4835c764f1b4cf28305782517b6cbc8c
munga <mwachilumobm@gmail.com>
2024-03-16 20:07:06 +0300

    chore: add database migration setup

--- ecosortify.py
+++ ecosortify.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ Defines flask application instance and tasks to manage app """
 import os
-from app import create_app
+from app import create_app, db
+from flask_migrate import Migrate
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+database_migrate = Migrate(app, db, include_schema=True)
 
 
 @app.shell_context_processor
